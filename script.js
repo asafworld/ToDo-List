2e900e249bf38c42b11bb6658ee676fe7449c8f2
asafworld <gabrielasaf10@gmail.com>
2022-02-16 16:08:29 -0300

    Adiciona alguns reparos

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ const taskList = document.createElement('ol');
 taskList.id = 'lista-tarefas';
 main.appendChild(taskList);
 
-let inputValue = input.value;
-
 const buttonCriaTarefa = document.createElement('button');
 buttonCriaTarefa.id = 'criar-tarefa';
 buttonCriaTarefa.innerHTML = 'Cria Tarefa';
 main.appendChild(buttonCriaTarefa);
 
-buttonCriaTarefa.addEventListener('click', function(event){
-    let liTarefa = document.createElement('li');
-    liTarefa.id = 'lista-tarefas';
-    liTarefa.innerHTML = input.value;
-    taskList.appendChild(liTarefa);
-    input.value = '';
+buttonCriaTarefa.addEventListener('click', function(event) {
+  const liTarefa = document.createElement('li');
+  liTarefa.innerHTML = input.value;
+  taskList.appendChild(liTarefa);
+  input.value = '';
 })
 
+let liTarefas = document.getElementsByTagName('li');
 
+function addBackgroundColorLi() {
+  for (let i = 0; i < liTarefas.length; i += 1){
+    liTarefas[i].addEventListener('click', function(event) {
+      liTarefas[i].classList.add('liStyle');
+    });
+  };
+};
+addBackgroundColorLi();
