e7328796353ddc0368af2f89f333d4868ec94643
asafworld <gabrielasaf10@gmail.com>
2022-02-16 21:08:09 -0300

    Adiciona requisito 11

--- script.js
+++ script.js
@@ -42,11 +42,25 @@ deleteButton.innerHTML = 'Apagar tudo'
 main.appendChild(deleteButton);
 
 function deleteItAll(){
-    const lines = document.querySelectorAll('li');
-    for (let i = 0; i < lines.length; i +=1) {
-        lines[i].remove();
-    };
+  const lines = document.querySelectorAll('li');
+  for (let i = 0; i < lines.length; i +=1) {
+      lines[i].remove();
+  };
 };
+deleteItAll
 
 deleteButton.addEventListener('click', deleteItAll);
 
+const deleteButtonFinalizados = document.createElement('button');
+deleteButtonFinalizados.id = 'remover-finalizados';
+deleteButtonFinalizados.innerHTML = 'Apagar finalizados';
+main.appendChild(deleteButtonFinalizados);
+
+function deleteFinalizados(){
+    const finalizados = document.querySelectorAll('.completed');
+    for (let i = 0; i < finalizados.length; i += 1){
+      finalizados[i].remove();
+    }
+};
+
+deleteButtonFinalizados.addEventListener('click', deleteFinalizados);
